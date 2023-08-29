54505019cc2ef1f0cf6f040fa69acc83335f3526
jeovanasantos <you@example.com>
2023-08-29 10:16:38 -0400

    atribuição de item a lista

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const express = require('express')
+const app = express()
+
+//End point(rotas)principal
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/oi', function (req, res) {
+  res.send('Ola mundo')
+});
+
+//endpoint de herois
+const lista =["Mulher Maravilha", "Capitã Marvel", "Homem de ferro"];
+
+//Read all -> GET /herois
+app.get("/herois", function(req,res) {
+  res.send(lista);
+});
+app.listen(3000)
